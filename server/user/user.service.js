a59a025966af5503a973f05f09ef0e9f8396d656
Mehrdad Helmi <mehrdadhelmi.biz@gmail.com>
2023-06-26 05:28:55 +0330

    feat(server):
    user services added

---
+++ server/user/user.service.js
@@ -0,0 +1,42 @@
+const db = require('../_database'),
+	User = db.User;
+const config = require('../app.config.json');
+
+async function signup(bodyUser) {
+	try{
+		const user = new User(bodyUser);
+		return user.save();
+	}catch (err) {
+		return err;
+	}
+}
+
+
+async function login(name) {
+	return new Promise((resolve, reject) => {
+		User.findOne({name}).exec(async (err, user) => {
+			if (err) {
+				return reject(err);
+			}
+
+			if (!user) {
+				return reject({
+					name: 'customError',
+					message: 'name is incorrect..',
+					code: 400,
+				});
+			}
+			
+			resolve({
+				_id: user._id,
+				name: user.name
+			});
+		});
+	});
+
+}
+
+module.exports = {
+	signup,
+	login,
+};
